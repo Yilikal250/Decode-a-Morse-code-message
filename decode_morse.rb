6cd9fefc4a924336aa6baacfd0a1afe3b5d4f03a
AntonioHincapie <marcohincapiem@gmail.com>
2022-08-04 14:18:29 -0500

    possible solution

--- decode_morse.rb
+++ decode_morse.rb
@@ -42,4 +42,63 @@ end
 
 decode_morse('-- -.--   -. .- -- .')
 
+# Antonio's code
 
+MORSE_DICT = Hash[
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z'
+]
+
+def decode_char(char)
+  @char = ''
+  MORSE_DICT.each do |key, value|
+    @char = value if key == char
+  end
+  @char
+end
+
+def decode_word(word)
+  @full_word = word.split
+  @word = ''
+  @full_word.each do |morse_code|
+    @word += decode_char(morse_code).to_s
+  end
+  @word
+end
+
+def decode(message)
+  @full_message = message.split('   ')
+  @final_message = ''
+  @full_message.each do |morse_code_message|
+    @final_message += "#{decode_word(morse_code_message)}" + " "
+  end
+  @final_message
+end
+
+puts decode_char('.-').upcase
+puts decode_word('-- -.--').upcase
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...').upcase
